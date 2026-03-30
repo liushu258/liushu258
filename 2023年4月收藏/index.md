06c7d362147f25a280d6b51b46058e266eff5778
liushu258 <>
2026-03-30 14:36:48 +0000

    Update from https://github.com/liushu258/xiaoliu/commit/7ca790174fd2c205c6570bdf63e32869b68bea3d

--- 2023年4月收藏/index.md
+++ 2023年4月收藏/index.md
@@ -9,7 +9,7 @@
 
 ```
 OCR：
-ExtractPDF.com(https://www.extractpdf.com/zh.html)从PDF文件中提取图像，文本和字体
+	ExtractPDF.com(https://www.extractpdf.com/zh.html)从PDF文件中提取图像，文本和字体
 
 ```
 
@@ -28,43 +28,71 @@ Discard使用Midjourney 绘制你自己的皮克斯风格头像
 
 
 
-## 2023年4月3日
-
-### 前端 Github
-
-
-
-## 2023年4月4日
-
-### 前端 Github
-
-
-
-## 2023年4月5日
-
-### 前端 Github
-
-
-
-## 2023年4月6日
-
-
-
 ## 2023年4月7日
 
+### 资源
 
+```
+全球 10 大片源 BT 网站
+海盗湾： https://thepiratebay.org/
+1337X：https://1337x.to/
+EZTV： https://eztv.re/
+YTS： https://yts.mx/
+RARBG: https://rarbg.to/
+恶魔网： https://www.demonoid.is/
+Nyaa: https://nyaa.si/
+BT4G：https://bt4g.org/
+magnetDL： www.magnetdl.com/
+```
 
 
 
 ## 2023年4月8日
 
+### 打卡
+
+```
+石油化工第四章
+```
+
+### 前端
+
+```
+CSS 工具
+    图表 https://chartscss.org/
+    按钮 https://www.bestcssbuttongenerator.com/
+    网格 http://grid.guide/
+    图案 https://css-doodle.com/
+    组件 https://css-challenges.com/
+前端精读系列文章，每周更新
+    https://github.com/ascoders/weekly/issues/2	
+    http://www.h-camel.com/index.html
+AI
+	https://github.com/yzfly/awesome-chatgpt-zh
+```
+
 
 
 ## 2023年4月9日
 
+### 打卡
+
+```
+石油化工第五章
+WebAPI接口对接会
+人啊就不能太善良
+好喜欢梦川啊 今年定个小目标:去听一场梦川的现场
+```
+
 
 
 ## 2023年4月10日
+
+### 打卡
+
+```
+
+```
 
 
 
@@ -90,7 +118,7 @@ Discard使用Midjourney 绘制你自己的皮克斯风格头像
 
 ```
 
-## 2023年4月15日
+## df2023年4月15日
 
 ### 打卡
 
--- js/custom.js
+++ js/custom.js
@@ -1,46 +1,40 @@
-/* 控制台输出 */
+﻿/* 鎺у埗鍙拌緭鍑?*/
 if(window.console) {
 	var cons = console;
 	if(cons) {
-		cons.group("O(∩_∩)O哈喽！");
+		cons.group("O(鈭鈭?O鍝堝柦锛?);
 		cons.info("");
 		cons.log("");
-		cons.info("==============================此乃分界线==============================");
+		cons.info("==============================姝や箖鍒嗙晫绾?=============================");
 		cons.log("")
 		cons.groupEnd();
 	}
 }
 
-/* 轮播背景图片 */
-$(function () {
-	$.backstretch([
-		"/liushu258/images/background/bulb.png",
-		"/liushu258/images/background/christmas.png",
-		"/liushu258/images/background/black-curve.png",
-		// "/liushu258/images/background/binary.png",
-	], { duration: 6000, fade: 1500 });
-});
+/* 杞挱鑳屾櫙鍥剧墖 */
+/* CSS background is handled in assets/css/_custom.scss */
 
-/* 返回随机颜色 */
+/* 杩斿洖闅忔満棰滆壊 */
 function randomColor() {
 	return "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")";
 }
 
-/* 鼠标点击文字特效 */
+/* 榧犳爣鐐瑰嚮鏂囧瓧鐗规晥 */
 var a_idx = 0;
 var a_click = 1;
-var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善",
-			"老哥稳", "带我飞", "厉害了word哥", "扎心了老铁", "蓝瘦香菇", "还有这种操作?", "就是有这种操作",
-			"皮皮虾我们走", "笑到猪叫", "石乐志", "不存在的", "黑车!", "我要下车!", "他还只是个孩子", "请不要放过他",
-			"惊不惊喜?", "意不意外?", "我有一个大胆的想法", "你的良心不会痛吗", "你心里就没点b数吗", "没有,我很膨胀",
-			"秀", "天秀", "陈独秀", "蒂花之秀", "造化钟神秀", "我去买几个橘子", "你就站在此地", "不要走动",
-			"我可能读了假书", "贫穷限制了我的想象力", "打call", "当然是选择原谅她啊", "你有freestyle吗",
-			"北大还行撒贝宁", "不知妻美刘强东", "悔创阿里杰克马", "一无所有王健林", "普通家庭马化腾",
-			"别点了", "求求你别点了", "你还点", "你再点!", "有本事继续点!", "你厉害", "我投翔",
-			"w(·Д·)w", "(#`O′)", "（/TДT)/", "┭┮﹏┭┮", "_(:3」∠)_");
+var a = new Array("浠庢浠ュ悗涓嶈皥鍦颁箙澶╅暱锛屽崟鍒€璧翠細鍋氭皵璐ㄦ祦姘撱€?, "鍏堢┛琚滃瓙鍐嶇┛闉嬶紝鍏堝綋瀛欏瓙鍦ㄥ綋鐖凤紝鐭ラ亾浠€涔堝彨鐖风埛鍚楋紝鐖哥埜鐨勭埜鐖稿氨鍙埛", "浣犲杩芥垜鍗佹潯琛楋紝璇存垜闀垮緱鍍忎綘鐖癸紒", "绀句細鐜板疄涓€鍦烘垙锛岀帺鐨勯兘鏄汉姘戝竵锛?,
+			"涓€涓汉70鏂わ紝鍘诲帟鎵€鎷夊畬灞庡洖鏉ワ紝涓轰粈涔堣繕鏄?0鏂?鎷夎￥瀛愪笂浜嗐€?, "骞崇瓑", "鍏" ,"娉曟不", "鐖卞浗", "鏁笟", "璇氫俊", "鍙嬪杽",
+			"鑰佸摜绋?, "甯︽垜椋?, "鍘夊浜唚ord鍝?, "鎵庡績浜嗚€侀搧", "钃濈槮棣欒弴", "杩樻湁杩欑鎿嶄綔?", "灏辨槸鏈夎繖绉嶆搷浣?,
+			"鐨毊铏炬垜浠蛋", "绗戝埌鐚彨", "鐭充箰蹇?, "涓嶅瓨鍦ㄧ殑", "榛戣溅!", "鎴戣涓嬭溅!", "浠栬繕鍙槸涓瀛?, "璇蜂笉瑕佹斁杩囦粬",
+			"鎯婁笉鎯婂枩?", "鎰忎笉鎰忓?", "鎴戞湁涓€涓ぇ鑳嗙殑鎯虫硶", "浣犵殑鑹績涓嶄細鐥涘悧", "浣犲績閲屽氨娌＄偣b鏁板悧", "娌℃湁,鎴戝緢鑶ㄨ儉",
+			"绉€", "澶╃", "闄堢嫭绉€", "钂傝姳涔嬬", "閫犲寲閽熺绉€", "鎴戝幓涔板嚑涓瀛?, "浣犲氨绔欏湪姝ゅ湴", "涓嶈璧板姩",
+			"鎴戝彲鑳借浜嗗亣涔?, "璐┓闄愬埗浜嗘垜鐨勬兂璞″姏", "鎵揷all", "褰撶劧鏄€夋嫨鍘熻皡濂瑰晩", "浣犳湁freestyle鍚?,
+			"鍖楀ぇ杩樿鎾掕礉瀹?, "涓嶇煡濡荤編鍒樺己涓?, "鎮斿垱闃块噷鏉板厠椹?, "涓€鏃犳墍鏈夌帇鍋ユ灄", "鏅€氬搴┈鍖栬吘",
+			"鍒偣浜?, "姹傛眰浣犲埆鐐逛簡", "浣犺繕鐐?, "浣犲啀鐐?", "鏈夋湰浜嬬户缁偣!", "浣犲帀瀹?, "鎴戞姇缈?,
+			"w(路袛路)w", "(#`O鈥?", "锛?T袛T)/", "鈹敭锕忊敪鈹?, "_(:3銆嶁垹)_");
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-		/* 点击频率，点击几次就换文字 */
+		/* 鐐瑰嚮棰戠巼锛岀偣鍑诲嚑娆″氨鎹㈡枃瀛?*/
 		var frequency = 2;
 		if (a_click % frequency === 0) {
 
@@ -76,7 +70,7 @@ jQuery(document).ready(function($) {
     });
 });
 
-/* 拉姆蕾姆回到顶部或底部按钮 */
+/* 鎷夊钑惧鍥炲埌椤堕儴鎴栧簳閮ㄦ寜閽?*/
 $(function() {
 	$("#lamu img").eq(0).click(function() {
 		$("html,body").animate({scrollTop:$(document).height()},800);
@@ -88,15 +82,6 @@ $(function() {
 	});
 });
 
-/* 后置加载页面组件的背景图片 */
-// $(function() {
-// 	/* 首页头像div加载GitHub Chart作为背景图片 */
-// 	$("div.home-avatar").attr('style', "background: url(https://ghchart.rshah.org/FFA500/lewky);background-repeat: no-repeat;background-position: center;background-size: auto 7.5rem;");
-
-// 	/* 评论框加载背景图片 */
-// 	$(".v[data-class=v] .veditor").attr('style', "background-image: url(" + $cdnPrefix + "/images/common/valinebg.webp) !important;");
-// });
-
 function getCurrentDateString() {
 	var now = new Date();
 	var month = now.getMonth() + 1;
@@ -105,7 +90,7 @@ function getCurrentDateString() {
 	return "" + now.getFullYear() + (month < 10 ? "0" + month : month) + (day < 10 ? "0" + day : day) + (hour < 10 ? "0" + hour : hour);
 }
 
-/* 离开当前页面时修改网页标题，回到当前页面时恢复原来标题 */
+/* 绂诲紑褰撳墠椤甸潰鏃朵慨鏀圭綉椤垫爣棰橈紝鍥炲埌褰撳墠椤甸潰鏃舵仮澶嶅師鏉ユ爣棰?*/
 window.onload = function() {
   var OriginTitile = document.title;
   var titleTime;
@@ -113,12 +98,12 @@ window.onload = function() {
     if(document.hidden) {
       $('[rel="icon"]').attr('href', "/failure.ico");
       $('[rel="shortcut icon"]').attr('href', "/failure.ico");
-      document.title = '你别走鸭 ಥ_ಥ';
+      document.title = '浣犲埆璧伴腑 嗖嗖?;
       clearTimeout(titleTime);
     } else {
       $('[rel="icon"]').attr('href', "/favicon-32x32.png");
       $('[rel="shortcut icon"]').attr('href', "/favicon-32x32.png");
-      document.title = 'hi，你回来啦 Hi~ o(*￣▽￣*)ブ';
+      document.title = 'hi锛屼綘鍥炴潵鍟?Hi~ o(*锟ｂ柦锟?)銉?;
       titleTime = setTimeout(function() {
         document.title = OriginTitile;
       }, 2000);
@@ -126,10 +111,10 @@ window.onload = function() {
   });
 }
 
-/* 站点运行时间 */
+/* 绔欑偣杩愯鏃堕棿 */
 function runtime() {
 	window.setTimeout("runtime()", 1000);
-	/* 请修改这里的起始时间 */
+	/* 璇蜂慨鏀硅繖閲岀殑璧峰鏃堕棿 */
     let startTime = new Date('11/20/2022 15:00:00');
     let endTime = new Date();
     let usedTime = endTime - startTime;
@@ -141,10 +126,10 @@ function runtime() {
     let leavel3 = leavel2 % (60 * 1000);
     let seconds = Math.floor(leavel3 / (1000));
     let runbox = document.getElementById('run-time');
-    runbox.innerHTML = '本站已运行<i class="far fa-clock fa-fw"></i> '
-        + ((days < 10) ? '0' : '') + days + ' 天 '
-        + ((hours < 10) ? '0' : '') + hours + ' 时 '
-        + ((minutes < 10) ? '0' : '') + minutes + ' 分 '
-        + ((seconds < 10) ? '0' : '') + seconds + ' 秒 ';
+    runbox.innerHTML = '鏈珯宸茶繍琛?i class="far fa-clock fa-fw"></i> '
+        + ((days < 10) ? '0' : '') + days + ' 澶?'
+        + ((hours < 10) ? '0' : '') + hours + ' 鏃?'
+        + ((minutes < 10) ? '0' : '') + minutes + ' 鍒?'
+        + ((seconds < 10) ? '0' : '') + seconds + ' 绉?';
 }
 runtime();
